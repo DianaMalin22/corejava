615ce136f63bb8dd3e890138ffd9b1ab1b00c435
DianaMalin22 <diana.xworkz@gmail.com>
2021-06-24 23:42:07 +0530

    Basic Arithmetic operations programs

---
+++ AddingTwoNumbers.java
@@ -0,0 +1,13 @@
+class AddingTwoNumbers
+{
+public static void add(int a,int b){
+int num=a+b;
+System.out.println(num);
+}
+
+public static void main(String a[]){
+add(3,5);
+add(22,11);
+add(3456,7896);
+}
+}
